ed3a6edf7c86a7dfa49e122e95388caac51eff32
DonJoan <71422908+DonJoan@users.noreply.github.com>
2021-04-14 07:36:11 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -67,7 +67,7 @@ Citizen.CreateThread(function()
 		                                                vehicleKeys = CreateObject(GetHashKey("prop_cuff_keys_01"), 0, 0, 0, true, true, true) -- creates object
 	 	                                                AttachEntityToEntity(vehicleKeys, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 57005), 0.11, 0.03, -0.03, 90.0, 0.0, 0.0, true, true, false, true, 1, true) -- object is attached to right hand
 		                                                TaskPlayAnim(PlayerPedId(), dict, "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
-		                                                Wait(1400)
+		                                                Citizen.Wait(1400)
 	                                                end
 							SetVehicleLights(carstrie[i], 2)
 							Citizen.Wait(150)
@@ -85,7 +85,7 @@ Citizen.CreateThread(function()
 								vehicleKeys = CreateObject(GetHashKey("prop_cuff_keys_01"), 0, 0, 0, true, true, true) -- creates object
 	 	                                                AttachEntityToEntity(vehicleKeys, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 57005), 0.11, 0.03, -0.03, 90.0, 0.0, 0.0, true, true, false, true, 1, true) -- object is attached to right hand
 		                                                TaskPlayAnim(PlayerPedId(), dict, "fob_click_fp", 8.0, 8.0, -1, 48, 1, false, false, false)
-		                                                Wait(1400)
+		                                                Citizen.Wait(1400)
 	                                                end
 							SetVehicleLights(carstrie[i], 2)
 							Citizen.Wait(150)
